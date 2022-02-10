7224adf7451b165d37870d944b64b018085f5d7b
jinabro <kmy4869@naver.com>
2022-02-10 12:18:11 +0900

    AI 랜덤이동 구현

--- ArenaBattle/Source/ArenaBattle/ABAIController.cpp
+++ ArenaBattle/Source/ArenaBattle/ABAIController.cpp
@@ -2,4 +2,40 @@
 
 
 #include "ABAIController.h"
+#include "NavigationSystem.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
 
+AABAIController::AABAIController()
+{
+	RepeatInterval = 3.f;
+}
+
+void AABAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	GetWorld()->GetTimerManager().SetTimer(RepeatTimerHandle, this, &AABAIController::OnRepeatTimer, RepeatInterval, true);
+}
+
+void AABAIController::OnUnPossess()
+{
+	Super::OnUnPossess();
+	GetWorld()->GetTimerManager().ClearTimer(RepeatTimerHandle);
+}
+
+void AABAIController::OnRepeatTimer()
+{
+	auto CurrentPawn = GetPawn();
+	ABCHECK(nullptr != CurrentPawn);
+
+	UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetNavigationSystem(GetWorld());
+	if (nullptr == NavSystem)
+		return;
+
+	FNavLocation NextLocation;
+	if (NavSystem->GetRandomPointInNavigableRadius(FVector::ZeroVector, 500.f, NextLocation))
+	{
+		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, NextLocation.Location);
+		ABLOG(Warning, TEXT("Next Location : %s"), *NextLocation.Location.ToString());
+	}
+
+}
--- ArenaBattle/Source/ArenaBattle/ABAIController.h
+++ ArenaBattle/Source/ArenaBattle/ABAIController.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "ArenaBattle.h"
 #include "AIController.h"
 #include "ABAIController.generated.h"
 
@@ -13,5 +13,16 @@ UCLASS()
 class ARENABATTLE_API AABAIController : public AAIController
 {
 	GENERATED_BODY()
+
+public:
+	AABAIController();
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void OnUnPossess() override;
+
+private:
+	void OnRepeatTimer();
+
+	FTimerHandle RepeatTimerHandle;
+	float RepeatInterval;
 	
 };
